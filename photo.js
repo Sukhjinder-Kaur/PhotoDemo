5e049c2d0bf6c764edfc38a1552efd988bfb9a1c
Sukhjinder-Kaur <sukhmanyu@gmail.com>
2020-10-05 11:59:33 -0400

    Added main.js

--- photo.js
+++ photo.js
@@ -1,4 +1,4 @@
-class Photo {
+export class Photo {
     constructor(width = 8, height = 10){
         this.width = width;
         this.height = height;
@@ -24,7 +24,7 @@ class Photo {
         return `This is a ${this.width} by ${this.height} photo and it costs $${this.price()}`;
     }   
 }
-class MattedPhoto extends Photo{
+export class MattedPhoto extends Photo{
     constructor(width, height, color){
         super(width,height);
         this.color = color;
@@ -37,7 +37,7 @@ class MattedPhoto extends Photo{
         return `This is a ${this.width} by ${this.height} matted ${this.color} photo and it costs $${this.price()}`;
     }    
 }
-    class FramedPhoto extends Photo{
+export class FramedPhoto extends Photo{
     constructor(width, height, material,style){
         super(width,height);
         this.material = material
@@ -51,21 +51,3 @@ class MattedPhoto extends Photo{
         return `This is a ${this.width} by ${this.height} ${this.material} photo and it costs $${this.price()}.The style of the photo is ${this.style}.`;
     }    
 }
-
-let photo1 = new Photo();
-console.log(photo1.toString());
-
-let photo2 = new Photo(12, 15);
-console.log(photo2.toString());
-
-let mattedPhoto1 = new MattedPhoto(9,12,'Red');
-console.log(mattedPhoto1.toString());
-
-let mattedPhoto2 = new MattedPhoto(10,11,'White');
-console.log(mattedPhoto2.toString());
-
-let framedPhoto1 = new FramedPhoto(5,12,'silver','modern');
-console.log(framedPhoto1.toString());
-
-let framedPhoto2 = new FramedPhoto(7,12,'gold','classic');
-console.log(framedPhoto2.toString());
